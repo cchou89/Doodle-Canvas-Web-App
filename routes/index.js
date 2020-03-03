c3efca63d88c3762282ad31cf3ac0c973b2af139
dtahmasb <dtahmasb@sfu.ca>
2020-03-03 01:07:08 -0800

    Add login, register, logout pages. Add RESTful routing. Refactor to have VMC form. Add Model for user type Add Mongo DB Add Passport and local Authentication Add flash to keep important variables during routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var User = require("../models/user");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Skynet' });
+router.get('/', function(request, response, next) {
+  response.render('home');
+});
+
+/* show register form */
+router.get("/register", function(request, response){
+  response.render("register");
+});
+/* handle sign up logic */
+router.post("/register", function(request, response){
+  var newUser = new User({username: request.body.username});
+  User.register(newUser, request.body.password, function(err, user){
+    if(err){
+      request.flash("error", err.message);
+      return response.render("register");
+    }
+    passport.authenticate("local")(request, response, function(){
+      request.flash('message', 'Welcome to Skynet '+ user.username);
+      response.redirect("/");
+    });
+  });
+});
+
+/* show login form */
+router.get("/login", function(request, response){
+  response.render('login');
+});
+//handling login logic
+router.post("/login", passport.authenticate("local",
+    {
+      successRedirect: "/users",
+      failureRedirect: "/login"
+    }), function(request, response){
+});
+
+
+// logout route
+router.get("/logout", function(request, response){
+  request.logout();
+  request.flash('message', 'Logged out');
+  response.redirect("/users");
 });
 
 module.exports = router;
+
+
+
+
