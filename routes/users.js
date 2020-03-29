07e8c45963e56771a40abfc4bb4ef8d5e26d0b37
Damoon Tahmasbi <dtahmasb@sfu.ca>
2020-03-28 21:44:55 -0700

    Add users/edit Add delete user form    deleting user will delete all lectures of the user as well

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var Lecture = require('../models/lecture');
+var authenticate = require('../middleware/authenticate');
 
 /* GET users listing. */
 router.get('/', function(request, response) {
@@ -33,5 +34,61 @@ router.get('/:id', function (request, response) {
     }
   });
 });
+/* EDIT: GET the edit page */
+router.get("/:id/edit", authenticate.isLoggedIn, function(request, response){
+  User.findById(request.params.id, function (error, foundUser) {
+    if(error){
+      request.flash('error', 'could not find the user');
+      response.redirect("/users/" + request.params.id);
+    }
+    response.render('users/edit', {user : foundUser});
+  });
+});
+/* UPDATE: PUT the edit page */
+/* @TODO only checks is the user is logged in, does not check ownership of the account */
+router.put('/:id', authenticate.isLoggedIn, function (request, response) {
+  User.findById(request.params.id, function (error, foundUser) {
+    if(error){
+      request.flash('error', 'something went wrong');
+      response.redirect("/users/" + request.params.id);
+    } else {
+      console.log(request.body);
+      foundUser.username = request.body.username;
+      foundUser.save();
+      response.redirect("/users" );
+    }
+  });
+});
+// /* DESTROY: DELETE a user*/
+// /* @TODO only checks is the user is logged in, does not check ownership of the account */
+router.delete("/:id", authenticate.isLoggedIn, function (request, response) {
+  /* first destroy all lectures owned by the user */
+  Lecture.find({'author': request.params.id }, function (error, lectures) {
+    if (error){
+      request.flash('error', "Error searching for the user's lectures");
+      response.redirect('/');
+    } else {
+      lectures.forEach(function(lecture){
+        Lecture.findByIdAndDelete(lecture._id, function (error) {
+          if (error) {
+            console.log('could not delete all lectures');
+            request.flash('error', 'could not delete all lectures of the user');
+            response.redirect("/users/" + request.params.id);
+          }
+        });
+      });
+    }
+  });
+    /* then delete the user */
+  User.findByIdAndDelete(request.params.id, function (error) {
+      if (error) {
+        request.flash('error', 'could not delete the user');
+        response.redirect("/users");
+      } else {
+        response.redirect("/users");
+      }
+    });
+});
+
 
 module.exports = router;
