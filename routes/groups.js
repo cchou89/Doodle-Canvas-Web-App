ec53db8a28452685cbde7dea8c8dcbeec67660ff
ckc21 <ckc21@sfu.ca>
2020-03-31 02:07:02 -0700

    Add feature to update db with group id Add TODO for querying users Switch url to local for testing

--- routes/groups.js
+++ routes/groups.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var Group = require('../models/group');
 var User = require('../models/user');
 var authenticate = require('../middleware/authenticate');
@@ -26,12 +27,30 @@ router.get("/new", authenticate.isLoggedIn, function(request, response){
 /* CREATE: POST Group form that creates a new Group */
 router.post('/new', authenticate.isLoggedIn, function(request,response) {
         var name = request.body.groupName;
+        var user = request.user;
         var owner = request.user._id;
         var members = [request.body.memberInput];
-        var newGroup = {name: name,
+        var _id= mongoose.Types.ObjectId();
+        var list = [];
+
+        //TODO: Query list of members and add to list
+        members.forEach(function(member){
+           var query = User.findOne({name:member}, '*', function(error){
+               if(error){
+                   console.log('Cannot find %s', name);
+               } else{
+                   list.push(query);
+               }
+           });
+        });
+
+        var newGroup = {
+                        _id: _id,
+                        name: name,
                         owner:owner,
-                        members:members
+                        members:list
                         };
+        //Create group
         Group.create(newGroup, function (error) {
             if(error){
                 request.flash('error', "Could not create the group");
@@ -39,7 +58,23 @@ router.post('/new', authenticate.isLoggedIn, function(request,response) {
             } else {
                 //redirect back to index
                 response.redirect("/groups");                }
-        })
+        });
+
+        //Update user
+        user.update(
+        {
+            $push: {
+                groups: {
+                $each:[{_id}]}
+            }
+            }
+        );
+
+
+
+
+
+
 });
 
 /* SHOW: GET a group */
