323f9ca82a7cbdefdb7b77aa96343fad95a4bb88
ckc21 <ckc21@sfu.ca>
2020-04-04 01:33:56 -0700

    Clean up code Remove console.log() commands Remove dead code Edit redirect to direct to group page

--- routes/groups.js
+++ routes/groups.js
@@ -27,7 +27,7 @@ router.get("/new", authenticate.isLoggedIn, function(request, response){
 /* SHOW: GET a group */
 router.get("/:id", function (request, response) {
     console.log(request.params.id);
-    var doc = Group.findById(request.params.id).exec(function (error, foundGroup){
+    Group.findById(request.params.id).exec(function (error, foundGroup){
         if(error){
             console.log(error);
             request.flash('error', 'could not find the group');
@@ -56,7 +56,6 @@ module.exports = router;
 
 /* CREATE: POST Group form that creates a new Group */
 router.post('/new', authenticate.isLoggedIn, function(request,response) {
-    console.log(request.body.member[0]);
     var name = request.body.groupName;
     var owner = request.user._id;
     var memberList= request.body.member;
@@ -65,19 +64,15 @@ router.post('/new', authenticate.isLoggedIn, function(request,response) {
     User.find({username: memberList})
         .exec()
         .then(function(result) {
-            console.log("We start here");
-            console.log(result);
             return Promise.all(result)
                 .then(function addId(user) {
-                    console.log("step 2");
-                    console.log(user);
                     list = user.map(user => mongoose.Types.ObjectId(user._id.toString()));
-                    console.log(list);
                     return list;
                 })
         })
         .then(function fillSchema(list) {
             return {
+                _id: mongoose.Types.ObjectId(),
                 name: name,
                 owner: owner,
                 members: list
@@ -85,27 +80,17 @@ router.post('/new', authenticate.isLoggedIn, function(request,response) {
         })
         .then(function createGroup(newGroup){
             //Create group
-            Group.create(newGroup, function (error) {
-                response.redirect("/groups");
+            Group.create(newGroup, function(error){
+                if(error){
+                    throw error;
+                }else{
+                    response.redirect("/groups/" + newGroup._id);
+                }
             });
-        }).catch(function(error){
+        })
+        .catch(function(error){
         request.flash('error', "Could not create the group");
         //redirect back to index
         response.redirect("/groups/new");
     });
-
-
-    // member1.forEach(function(member){
-    //     if(member !== ""){
-    //         User.find({username: member}, function (error, user) {
-    //             console.log(user);
-    //             if (error) {
-    //                 console.log('cannot find %s', member)
-    //             } else {
-    //                 console.log(user._id.toString());
-    //                 list.push(user._id.toString());
-    //             }
-    //         });
-    //     }
-    // });
 });
