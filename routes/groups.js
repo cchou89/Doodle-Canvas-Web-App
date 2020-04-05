d1270249990791b557a576834a98a5cc934b2b31
ckc21 <ckc21@sfu.ca>
2020-04-04 20:28:46 -0700

    Update edit.ejs to match new.ejs Update edit function

--- routes/groups.js
+++ routes/groups.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var Group = require('../models/group');
+var User = require('../models/user');
 var authenticate = require('../middleware/authenticate');
 var router = express.Router();
 
@@ -22,27 +24,9 @@ router.get("/new", authenticate.isLoggedIn, function(request, response){
         response.render('groups/new');
 });
 
-/* CREATE: POST Group form that creates a new Group */
-router.post('/new', authenticate.isLoggedIn, function(request,response) {
-        var name = request.body.groupName;
-        var owner = request.user._id;
-        var members = [request.body.memberInput];
-        var newGroup = {name: name,
-                        owner:owner,
-                        members:members
-                        };
-        Group.create(newGroup, function (error) {
-            if(error){
-                request.flash('error', "Could not create the group");
-                response.redirect("/groups/new");
-            } else {
-                //redirect back to index
-                response.redirect("/groups");                }
-        })
-});
-
 /* SHOW: GET a group */
 router.get("/:id", function (request, response) {
+    console.log(request.params.id);
     Group.findById(request.params.id).exec(function (error, foundGroup){
         if(error){
             console.log(error);
@@ -51,8 +35,102 @@ router.get("/:id", function (request, response) {
         } else {
             response.render('groups/show', {group : foundGroup});
         }
-    })
+    });
 });
 
+/*EDIT group*/
+/* INDEX:  GET Group listing. */
+router.get('/:id/edit', authenticate.GroupOwnership, function(request, response) {
+// find the list of all Groups
+    Group.findById(request)
+        .then(function (error, list) {
+            response.render('groups/edit', {group: list});
+        })
+        .catch(function (error) {
+            request.flash('error', "Could not find the Group");
+            response.redirect('/groups'
+            );
+        });
+});
+
+// UPDATE: PUT the edit page
+router.put('/:id', authenticate.GroupOwnership, function (request, response) {
+    var memberList= request.body.member;
+    var list = [];
+    Lecture.findById(request.params.id)
+        .then( function (error, foundLecture) {
+            /* this update is not safe but works for now
+            *  @TODO can be improved by using findByIdAndUpdate, or Buffer.alloc() */
+            foundLecture.name = request.body.groupName;
+            User.find({username: memberList})
+                .exec()
+                .then(function(result) {
+                    return Promise.all(result)
+                        .then(function addId(user) {
+                            list = user.map(user => mongoose.Types.ObjectId(user._id.toString()));
+                            console.log(list);
+                            return list;
+                        })
+                        .catch(function(error) {
+                            request.flash('error', "Could not create the group");
+                            //redirect back to index
+                            response.redirect("/groups/new");
+                        })
+                        .then(function(list){
+                            foundLecture.members = foundLecture.members + list;
+                            foundLecture.save();
+                        });
+                })
+        .then(function(list){
+
+        });
+
+
+
+        foundLecture.save();
+        response.redirect("/groups/" + request.params.id);
+    });
+});
+
+/* CREATE: POST Group form that creates a new Group */
+router.post('/new', authenticate.isLoggedIn, function(request,response) {
+    var name = request.body.groupName;
+    var owner = request.user._id;
+    var memberList= request.body.member;
+    var list = [];
+
+    User.find({username: memberList})
+        .exec()
+        .then(function(result) {
+            return Promise.all(result)
+                .then(function addId(user) {
+                    list = user.map(user => mongoose.Types.ObjectId(user._id.toString()));
+                    return list;
+                })
+        })
+        .then(function fillSchema(list) {
+            return {
+                _id: mongoose.Types.ObjectId(),
+                name: name,
+                owner: owner,
+                members: list
+            };
+        })
+        .then(function createGroup(newGroup){
+            //Create group
+            Group.create(newGroup, function(error){
+                if(error){
+                    throw error;
+                }else{
+                    response.redirect("/groups/" + newGroup._id);
+                }
+            });
+        })
+        .catch(function(error){
+            request.flash('error', "Could not create the group");
+            //redirect back to index
+            response.redirect("/groups/new");
+        });
+});
 
 module.exports = router;
