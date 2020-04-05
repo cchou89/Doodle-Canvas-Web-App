ea4e856f85194fc0ce6997c997572248809360cf
Damoon Tahmasbi <dtahmasb@sfu.ca>
2020-04-05 00:06:34 -0700

    merged master back to the branch

--- routes/groups.js
+++ routes/groups.js
@@ -5,7 +5,7 @@ var authenticate = require('../middleware/authenticate');
 var router = express.Router();
 
 
-// /* INDEX:  GET Group listing. */
+/* INDEX:  GET Group listing. */
 router.get('/', function(request, response) {
     // find the list of all Groups
     Group.find({}, function (error, list) {
@@ -22,35 +22,6 @@ router.get('/', function(request, response) {
 router.get("/new", authenticate.isLoggedIn, function(request, response){
         response.render('groups/new');
 });
-
-/* SHOW: GET a group */
-router.get("/:id",  function (request, response) {
-    Group.findById(request.params.id).exec(function (error, foundGroup){
-        if(error){
-            console.log(error);
-            request.flash('error', 'could not find the group');
-            response.redirect('/groups');
-        } else {
-            response.render('groups/show', {group : foundGroup});
-        }
-    });
-});
-
-/* DESTROY: DELETE a group */
-router.delete("/:id", authenticate.groupOwnership ,  function (request, response) {
-    Group.findById(request.params.id,function () {
-        Group.findByIdAndDelete(request.params.id,function (error) {
-            if (error) {
-                request.flash('error', 'could not delete the group');
-                response.redirect("/groups");
-            } else {
-                response.redirect("/groups");
-            }
-        });
-    });
-});
-
-module.exports = router;
 
 /* CREATE: POST Group form that creates a new Group */
 router.post('/new', authenticate.isLoggedIn, function(request,response) {
@@ -87,8 +58,38 @@ router.post('/new', authenticate.isLoggedIn, function(request,response) {
             });
         })
         .catch(function(error){
-        request.flash('error', "Could not create the group");
-        //redirect back to index
-        response.redirect("/groups/new");
+            request.flash('error', "Could not create the group");
+            //redirect back to index
+            response.redirect("/groups/new");
+        });
+});
+
+/* SHOW: GET a group */
+router.get("/:id",  function (request, response) {
+    Group.findById(request.params.id).exec(function (error, foundGroup){
+        if(error){
+            console.log(error);
+            request.flash('error', 'could not find the group');
+            response.redirect('/groups');
+        } else {
+            response.render('groups/show', {group : foundGroup});
+        }
     });
 });
+
+/* DESTROY: DELETE a group */
+router.delete("/:id", authenticate.groupOwnership ,  function (request, response) {
+    Group.findById(request.params.id,function () {
+        Group.findByIdAndDelete(request.params.id,function (error) {
+            if (error) {
+                request.flash('error', 'could not delete the group');
+                response.redirect("/groups");
+            } else {
+                response.redirect("/groups");
+            }
+        });
+    });
+});
+
+module.exports = router;
+
